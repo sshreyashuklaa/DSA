6f9963f3909da0ccf7ce13ec35bf6a62bb071385
sshreyashuklaa <shreyashukla7868@gmail.com>
2026-07-12 01:17:13 +0530

    Added solutions to some basic pattern printing probelms

---
+++ Striver_A2Z/Basics/patternprinting.cpp
@@ -0,0 +1,122 @@
+/*
+*****
+
+*****
+
+*****
+
+*****
+
+*****
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter n: ";
+    cin>>n;
+
+     for(int i = 0; i<n; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                cout<<"*";
+            }
+            cout<<endl;
+        }
+}
+        */
+
+/*
+*
+
+**
+
+***
+
+****
+
+*****
+*/
+/*
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int n;
+cout<<"Enter the value of n: ";
+cin>>n;
+
+for(int i=0; i<n; i++)
+{
+    for(int j=0; j<=i; j++)
+    {
+        cout<<"*";
+    }
+    cout<<endl;
+}
+}*/
+
+/*
+1
+
+12
+
+123
+
+1234
+
+12345
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter the value of n: ";
+    cin>>n;
+
+     for(int i = 1; i<=n; i++)
+        {
+            for(int j=1; j<=i; j++)
+            {
+                cout<<j;
+            }
+            cout<<endl;
+        }
+}
+        */
+
+/*
+1
+
+22
+
+333
+
+4444
+
+55555
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter the value of n: ";
+    cin>>n;
+
+    for(int i=1; i<=n; i++)
+        {
+            for(int j=1; j<=i; j++)
+            {
+                cout<<i;
+            }
+            cout<<endl;
+        }
+}
